0afc7e11087bb2b7c68f86c8b9351511be70bd5a
KDMCrypto <dima.korepanov.05@list.ru>
2026-02-25 02:00:42 +0700

    lb3 code

---
+++ lab03/lab3/lab3/Form1.Designer.cs
@@ -0,0 +1,208 @@
+﻿namespace lab3
+{
+    partial class Form1
+    {
+        /// <summary>
+        ///  Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        ///  Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        ///  Required method for Designer support - do not modify
+        ///  the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            components = new System.ComponentModel.Container();
+            pictureBox1 = new PictureBox();
+            rows = new NumericUpDown();
+            columns = new NumericUpDown();
+            label1 = new Label();
+            label2 = new Label();
+            button1 = new Button();
+            timer1 = new System.Windows.Forms.Timer(components);
+            comboBox1 = new ComboBox();
+            temper = new NumericUpDown();
+            label3 = new Label();
+            label4 = new Label();
+            button2 = new Button();
+            ((System.ComponentModel.ISupportInitialize)pictureBox1).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)rows).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)columns).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)temper).BeginInit();
+            SuspendLayout();
+            // 
+            // pictureBox1
+            // 
+            pictureBox1.Location = new Point(32, 41);
+            pictureBox1.Name = "pictureBox1";
+            pictureBox1.Size = new Size(1000, 1000);
+            pictureBox1.TabIndex = 0;
+            pictureBox1.TabStop = false;
+            pictureBox1.Click += pictureBox1_Click;
+            pictureBox1.Paint += pictureBox1_Paint;
+            pictureBox1.MouseClick += pictureBox1_MouseClick;
+            // 
+            // rows
+            // 
+            rows.Increment = new decimal(new int[] { 2, 0, 0, 0 });
+            rows.Location = new Point(1187, 73);
+            rows.Maximum = new decimal(new int[] { 200, 0, 0, 0 });
+            rows.Minimum = new decimal(new int[] { 3, 0, 0, 0 });
+            rows.Name = "rows";
+            rows.Size = new Size(180, 31);
+            rows.TabIndex = 1;
+            rows.Value = new decimal(new int[] { 50, 0, 0, 0 });
+            rows.ValueChanged += rows_ValueChanged;
+            // 
+            // columns
+            // 
+            columns.Increment = new decimal(new int[] { 2, 0, 0, 0 });
+            columns.Location = new Point(1187, 123);
+            columns.Maximum = new decimal(new int[] { 200, 0, 0, 0 });
+            columns.Minimum = new decimal(new int[] { 3, 0, 0, 0 });
+            columns.Name = "columns";
+            columns.Size = new Size(180, 31);
+            columns.TabIndex = 2;
+            columns.Value = new decimal(new int[] { 50, 0, 0, 0 });
+            columns.ValueChanged += columns_ValueChanged;
+            columns.Paint += pictureBox1_Paint;
+            // 
+            // label1
+            // 
+            label1.AutoSize = true;
+            label1.Location = new Point(1053, 73);
+            label1.Name = "label1";
+            label1.Size = new Size(71, 25);
+            label1.TabIndex = 3;
+            label1.Text = "Строки";
+            // 
+            // label2
+            // 
+            label2.AutoSize = true;
+            label2.Location = new Point(1053, 125);
+            label2.Name = "label2";
+            label2.Size = new Size(84, 25);
+            label2.TabIndex = 4;
+            label2.Text = "Столбцы";
+            // 
+            // button1
+            // 
+            button1.Location = new Point(1106, 327);
+            button1.Name = "button1";
+            button1.Size = new Size(194, 82);
+            button1.TabIndex = 5;
+            button1.Text = "Старт/Стоп";
+            button1.UseVisualStyleBackColor = true;
+            button1.Click += button1_Click;
+            // 
+            // timer1
+            // 
+            timer1.Tick += timer1_Tick;
+            // 
+            // comboBox1
+            // 
+            comboBox1.FormattingEnabled = true;
+            comboBox1.Items.AddRange(new object[] { "Нет", "Север", "Юг", "Запад", "Восток" });
+            comboBox1.Location = new Point(1185, 227);
+            comboBox1.Name = "comboBox1";
+            comboBox1.Size = new Size(182, 33);
+            comboBox1.TabIndex = 6;
+            comboBox1.SelectedIndexChanged += Wind;
+            // 
+            // temper
+            // 
+            temper.Increment = new decimal(new int[] { 2, 0, 0, 0 });
+            temper.Location = new Point(1187, 174);
+            temper.Maximum = new decimal(new int[] { 50, 0, 0, 0 });
+            temper.Minimum = new decimal(new int[] { 50, 0, 0, int.MinValue });
+            temper.Name = "temper";
+            temper.Size = new Size(180, 31);
+            temper.TabIndex = 7;
+            temper.ValueChanged += temperature;
+            // 
+            // label3
+            // 
+            label3.AutoSize = true;
+            label3.Location = new Point(1053, 176);
+            label3.Name = "label3";
+            label3.Size = new Size(118, 25);
+            label3.TabIndex = 8;
+            label3.Text = "Температура";
+            // 
+            // label4
+            // 
+            label4.AutoSize = true;
+            label4.Location = new Point(1053, 230);
+            label4.Name = "label4";
+            label4.Size = new Size(58, 25);
+            label4.TabIndex = 9;
+            label4.Text = "Ветер";
+            // 
+            // button2
+            // 
+            button2.Location = new Point(1106, 450);
+            button2.Name = "button2";
+            button2.Size = new Size(194, 82);
+            button2.TabIndex = 10;
+            button2.Text = "Выход";
+            button2.UseVisualStyleBackColor = true;
+            button2.Click += button2_Click;
+            // 
+            // Form1
+            // 
+            AutoScaleDimensions = new SizeF(10F, 25F);
+            AutoScaleMode = AutoScaleMode.Font;
+            ClientSize = new Size(1379, 1242);
+            Controls.Add(button2);
+            Controls.Add(label4);
+            Controls.Add(label3);
+            Controls.Add(temper);
+            Controls.Add(comboBox1);
+            Controls.Add(button1);
+            Controls.Add(label2);
+            Controls.Add(label1);
+            Controls.Add(columns);
+            Controls.Add(rows);
+            Controls.Add(pictureBox1);
+            Name = "Form1";
+            Text = "Form1";
+            ((System.ComponentModel.ISupportInitialize)pictureBox1).EndInit();
+            ((System.ComponentModel.ISupportInitialize)rows).EndInit();
+            ((System.ComponentModel.ISupportInitialize)columns).EndInit();
+            ((System.ComponentModel.ISupportInitialize)temper).EndInit();
+            ResumeLayout(false);
+            PerformLayout();
+        }
+
+        #endregion
+
+        private PictureBox pictureBox1;
+        private NumericUpDown rows;
+        private NumericUpDown columns;
+        private Label label1;
+        private Label label2;
+        private Button button1;
+        private System.Windows.Forms.Timer timer1;
+        private ComboBox comboBox1;
+        private NumericUpDown temper;
+        private Label label3;
+        private Label label4;
+        private Button button2;
+    }
+}
---
+++ lab03/lab3/lab3/Form1.cs
@@ -0,0 +1,264 @@
+﻿using System.Runtime.CompilerServices;
+using System.Security.Cryptography.Xml;
+
+namespace lab3
+{
+    public partial class Form1 : Form
+    {
+        int[,] fields;
+
+        int cellSize, rowsCount, colsCount;
+        int[,] neighbourhood = { { -1, -1 }, { -1, 0 }, { -1, 1 }, { 0, -1 }, { 0, 1 }, { 1, -1 }, { 1, 0 }, { 1, 1 } };
+        int EMPTY = 0, TREE = 1, FIRE = 2, WATER = 3;
+        double[] NZ;
+        double tempFactor;
+        int p = 5, f = 2;
+
+
+
+        Random random = new Random();
+
+        private Brush[] colors = { Brushes.Brown, Brushes.Green, Brushes.Orange, Brushes.Blue }; // цвета для клеток
+        public Form1()
+        {
+            InitializeComponent();
+
+            this.DoubleBuffered = true;
+
+            comboBox1.SelectedIndex = 0;
+
+            UpdateGrid();
+        }
+
+        public int[,] Iteration(int[,] X)
+        {
+            int rows = X.GetLength(0);
+            int cols = X.GetLength(1);
+            int[,] X1 = new int[rows, cols];
+
+            // Константы вероятностей 
+            double p_d = p / 100.0; // p
+            double f_d = f / 100.0;     // f
+            double Diagonal_prob = 0.6;
+
+            for (int iy = 1; iy < rows - 1; iy++)
+            {
+                for (int ix = 1; ix < cols - 1; ix++)
+                {
+                    if (X[iy, ix] == WATER) X1[iy, ix] = WATER;
+
+                    else if (X[iy, ix] == EMPTY)
+                    {
+                        if (random.NextDouble() <= p_d)
+                            X1[iy, ix] = TREE;
+                    }
+
+                    else if (X[iy, ix] == TREE)
+                    {
+                        X1[iy, ix] = TREE; // По умолчанию остается деревом
+                        bool ignited = false;
+
+                        for (int i = 0; i < 8; i++)
+                        {
+                            int dy = neighbourhood[i, 0];
+                            int dx = neighbourhood[i, 1];
+
+                            // Если сосед горит — загораемся сами
+                            if (X[iy + dy, ix + dx] == FIRE && random.NextDouble() <= NZ[i] * tempFactor)
+                            {
+
+                                X1[iy, ix] = FIRE;
+                                ignited = true;
+                                break;
+                            }
+                        }
+
+                        // выполнится, если дерево не загорелось от соседей
+                        if (!ignited)
+                        {
+                            if (random.NextDouble() <= f_d)
+                            {
+                                X1[iy, ix] = FIRE;
+                            }
+                        }
+                    }
+                    // Горящее дерево превращается в пустое место
+                    else if (X[iy, ix] == FIRE)
+                    {
+                        X1[iy, ix] = EMPTY;
+                    }
+                }
+            }
+            return X1;
+        }
+
+        private void pictureBox1_Click(object sender, EventArgs e)
+        {
+        }
+        private void pictureBox1_Paint(object sender, PaintEventArgs e)
+        {
+            if (fields == null) return;
+
+            Graphics g = e.Graphics;
+
+            for (int i = 0; i < fields.GetLength(0); i++) // Строки
+            {
+                for (int j = 0; j < fields.GetLength(1); j++) // Столбцы
+                {
+                    g.FillRectangle(colors[fields[i, j]], j * cellSize, i * cellSize, cellSize, cellSize);
+                    g.DrawRectangle(Pens.Black, j * cellSize, i * cellSize, cellSize, cellSize);
+                }
+            }
+        }
+
+        private void button1_Click(object sender, EventArgs e)
+        {
+            if (timer1.Enabled == true)
+            {
+                timer1.Enabled = false;
+            }
+
+            else
+            {
+                timer1.Enabled = true;
+            }
+
+        }
+
+        private void UpdateGrid()
+        {
+            rowsCount = (int)rows.Value;
+            colsCount = (int)columns.Value;
+
+            if (rowsCount <= 0 || colsCount <= 0) return;
+
+            fields = new int[rowsCount, colsCount];
+            double forestFraction = 0.5; // заполнение лесом
+
+            // создание начальной карты
+            for (int iy = 1; iy < rowsCount - 1; iy++)
+            {
+                for (int ix = 1; ix < colsCount - 1; ix++)
+                {
+                    
+                    if (random.NextDouble() < forestFraction)
+                    {
+                        fields[iy, ix] = TREE; // Устанавливаем дерево
+                    }
+                    else
+                    {
+                        fields[iy, ix] = EMPTY; // Пусто
+                    }
+
+
+                }
+            }
+
+            // Добавляем озера
+            int numLakes = Math.Max(1, (rowsCount * colsCount) / 600);
+
+            for (int i = 0; i < numLakes; i++)
+            {
+                // Выбираем случайный центр озера
+                int cx = random.Next(3, colsCount - 3);
+                int cy = random.Next(3, rowsCount - 3);
+
+                // Случайный радиус озера
+                int radius = random.Next(2, 6);
+
+                // Проходим по квадрату вокруг центра озера
+                for (int dy = -radius; dy <= radius; dy++)
+                {
+                    for (int dx = -radius; dx <= radius; dx++)
+                    {
+                        // Проверяем, попадает ли клетка в круг (x^2 + y^2 <= r^2)
+                        if (dx * dx + dy * dy <= radius * radius && random.NextDouble() < 0.85)
+                        {
+                            int ny = cy + dy;
+                            int nx = cx + dx;
+
+                            // Защита от выхода за границы массива
+                            if (ny > 0 && ny < rowsCount - 1 && nx > 0 && nx < colsCount - 1)
+                            {
+                                fields[ny, nx] = WATER;
+                            }
+                        }
+                    }
+                }
+            }
+
+
+            // Вычисляем размер клетки, чтобы всё поле влезло в PictureBox
+            int sizeW = pictureBox1.Width / colsCount;
+            int sizeH = pictureBox1.Height / rowsCount;
+            cellSize = Math.Min(sizeW, sizeH); // Берем минимальное, чтобы клетки были квадратными
+
+            if (cellSize < 1) cellSize = 1;
+
+            pictureBox1.Invalidate();
+        }
+
+        //Учет ветра
+        public void Wind(object sender, EventArgs e)
+        {
+            NZ = new double[8];
+
+            for (int i = 0; i < 8; i++)
+            {
+
+                string direction = comboBox1.SelectedItem.ToString();
+
+                int NY = neighbourhood[i, 0];
+                int NX = neighbourhood[i, 1];
+
+                NZ[i] = 1;
+
+                if (direction == "Север" && NY > 0) NZ[i] = 0.1; // Ветер с севера затрудняет уход огня вниз
+                if (direction == "Юг" && NY < 0) NZ[i] = 0.1;   // Ветер с юга затрудняет уход огня вверх
+                if (direction == "Восток" && NX < 0) NZ[i] = 0.1; // Ветер с востока мешает идти влево
+                if (direction == "Запад" && NX > 0) NZ[i] = 0.1;  // Ветер с запада мешает идти вправо
+            }
+        }
+
+        public void temperature(object sender, EventArgs e)
+        {
+            double temperature_air = (double)temper.Value;
+            tempFactor = (temperature_air + 50) / 100.0;
+
+        }
+
+        // поджигание кликом мыши
+        private void pictureBox1_MouseClick(object sender, MouseEventArgs e)
+        {
+            int ix = e.X / cellSize;
+            int iy = e.Y / cellSize;
+
+            if (ix >= 0 && ix < colsCount && iy >= 0 && iy < rowsCount && fields[iy, ix] == TREE)
+            {
+                fields[iy, ix] = FIRE; // Поджигаем дерево кликом
+                pictureBox1.Invalidate();
+            }
+        }
+
+        private void rows_ValueChanged(object sender, EventArgs e)
+        {
+            UpdateGrid();
+        }
+
+        private void columns_ValueChanged(object sender, EventArgs e)
+        {
+            UpdateGrid();
+        }
+
+        private void timer1_Tick(object sender, EventArgs e)
+        {
+            fields = Iteration(fields);
+            pictureBox1.Invalidate();
+        }
+
+        private void button2_Click(object sender, EventArgs e)
+        {
+            this.Close();
+        }
+    }
+}
