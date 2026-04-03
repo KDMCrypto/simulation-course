ddf1406003b390cbf8442d4afd6719c1f386f26d
KDMCrypto <dima.korepanov.05@list.ru>
2026-04-04 03:08:11 +0700

    lb9 code

---
+++ lab09/lab9/lab9/Form1.Designer.cs
@@ -0,0 +1,196 @@
+﻿namespace lab9
+{
+    partial class Form1
+    {
+        /// <summary>
+        ///  Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        ///  Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        ///  Required method for Designer support - do not modify
+        ///  the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            System.Windows.Forms.DataVisualization.Charting.ChartArea chartArea1 = new System.Windows.Forms.DataVisualization.Charting.ChartArea();
+            System.Windows.Forms.DataVisualization.Charting.Legend legend1 = new System.Windows.Forms.DataVisualization.Charting.Legend();
+            System.Windows.Forms.DataVisualization.Charting.Series series1 = new System.Windows.Forms.DataVisualization.Charting.Series();
+            start_button = new Button();
+            lambda_form = new NumericUpDown();
+            mu_form = new NumericUpDown();
+            label1 = new Label();
+            label3 = new Label();
+            label2 = new Label();
+            N_form = new NumericUpDown();
+            button1 = new Button();
+            chart1 = new System.Windows.Forms.DataVisualization.Charting.Chart();
+            listBox1 = new ListBox();
+            ((System.ComponentModel.ISupportInitialize)lambda_form).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)mu_form).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)N_form).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)chart1).BeginInit();
+            SuspendLayout();
+            // 
+            // start_button
+            // 
+            start_button.BackColor = Color.Green;
+            start_button.FlatStyle = FlatStyle.Popup;
+            start_button.Font = new Font("Segoe UI", 10F, FontStyle.Bold, GraphicsUnit.Point, 204);
+            start_button.ForeColor = SystemColors.Window;
+            start_button.Location = new Point(923, 729);
+            start_button.Name = "start_button";
+            start_button.Size = new Size(112, 46);
+            start_button.TabIndex = 0;
+            start_button.Text = "Старт";
+            start_button.UseVisualStyleBackColor = false;
+            start_button.Click += start_button_Click;
+            // 
+            // lambda_form
+            // 
+            lambda_form.DecimalPlaces = 1;
+            lambda_form.Location = new Point(357, 36);
+            lambda_form.Name = "lambda_form";
+            lambda_form.Size = new Size(86, 31);
+            lambda_form.TabIndex = 1;
+            lambda_form.Value = new decimal(new int[] { 5, 0, 0, 0 });
+            // 
+            // mu_form
+            // 
+            mu_form.DecimalPlaces = 1;
+            mu_form.Location = new Point(357, 92);
+            mu_form.Name = "mu_form";
+            mu_form.Size = new Size(86, 31);
+            mu_form.TabIndex = 2;
+            mu_form.Value = new decimal(new int[] { 6, 0, 0, 0 });
+            // 
+            // label1
+            // 
+            label1.AutoSize = true;
+            label1.Location = new Point(23, 38);
+            label1.Name = "label1";
+            label1.Size = new Size(295, 25);
+            label1.TabIndex = 3;
+            label1.Text = "Интенсивность входящего потока:";
+            // 
+            // label3
+            // 
+            label3.AutoSize = true;
+            label3.Location = new Point(23, 94);
+            label3.Name = "label3";
+            label3.Size = new Size(261, 25);
+            label3.TabIndex = 5;
+            label3.Text = "Интенсивность обслуживания:";
+            // 
+            // label2
+            // 
+            label2.AutoSize = true;
+            label2.Location = new Point(23, 151);
+            label2.Name = "label2";
+            label2.Size = new Size(172, 25);
+            label2.TabIndex = 7;
+            label2.Text = "Количество заявок:";
+            // 
+            // N_form
+            // 
+            N_form.Location = new Point(357, 149);
+            N_form.Maximum = new decimal(new int[] { 10000, 0, 0, 0 });
+            N_form.Minimum = new decimal(new int[] { 1, 0, 0, 0 });
+            N_form.Name = "N_form";
+            N_form.Size = new Size(86, 31);
+            N_form.TabIndex = 6;
+            N_form.Value = new decimal(new int[] { 1000, 0, 0, 0 });
+            // 
+            // button1
+            // 
+            button1.BackColor = Color.Red;
+            button1.FlatStyle = FlatStyle.Popup;
+            button1.Font = new Font("Segoe UI", 10F, FontStyle.Bold, GraphicsUnit.Point, 204);
+            button1.ForeColor = SystemColors.Window;
+            button1.Location = new Point(995, 23);
+            button1.Name = "button1";
+            button1.Size = new Size(40, 40);
+            button1.TabIndex = 8;
+            button1.Text = "X";
+            button1.UseVisualStyleBackColor = false;
+            button1.Click += button1_Click;
+            // 
+            // chart1
+            // 
+            chartArea1.Name = "ChartArea1";
+            chart1.ChartAreas.Add(chartArea1);
+            legend1.Name = "Legend1";
+            chart1.Legends.Add(legend1);
+            chart1.Location = new Point(23, 210);
+            chart1.Name = "chart1";
+            series1.ChartArea = "ChartArea1";
+            series1.Legend = "Legend1";
+            series1.Name = "Series1";
+            chart1.Series.Add(series1);
+            chart1.Size = new Size(623, 565);
+            chart1.TabIndex = 9;
+            chart1.Text = "chart1";
+            // 
+            // listBox1
+            // 
+            listBox1.FormattingEnabled = true;
+            listBox1.ItemHeight = 25;
+            listBox1.Location = new Point(671, 210);
+            listBox1.Name = "listBox1";
+            listBox1.Size = new Size(364, 179);
+            listBox1.TabIndex = 10;
+            // 
+            // Form1
+            // 
+            AutoScaleDimensions = new SizeF(10F, 25F);
+            AutoScaleMode = AutoScaleMode.Font;
+            ClientSize = new Size(1047, 798);
+            Controls.Add(listBox1);
+            Controls.Add(chart1);
+            Controls.Add(button1);
+            Controls.Add(label2);
+            Controls.Add(N_form);
+            Controls.Add(label3);
+            Controls.Add(label1);
+            Controls.Add(mu_form);
+            Controls.Add(lambda_form);
+            Controls.Add(start_button);
+            Name = "Form1";
+            Text = "Form1";
+            ((System.ComponentModel.ISupportInitialize)lambda_form).EndInit();
+            ((System.ComponentModel.ISupportInitialize)mu_form).EndInit();
+            ((System.ComponentModel.ISupportInitialize)N_form).EndInit();
+            ((System.ComponentModel.ISupportInitialize)chart1).EndInit();
+            ResumeLayout(false);
+            PerformLayout();
+        }
+
+        #endregion
+
+        private Button start_button;
+        private NumericUpDown lambda_form;
+        private NumericUpDown mu_form;
+        private Label label1;
+        private Label label3;
+        private Label label2;
+        private NumericUpDown N_form;
+        private Button button1;
+        private System.Windows.Forms.DataVisualization.Charting.Chart chart1;
+        private ListBox listBox1;
+    }
+}
---
+++ lab09/lab9/lab9/Form1.cs
@@ -0,0 +1,132 @@
+﻿namespace lab9
+{
+    public partial class Form1 : Form
+    {
+        Random rnd = new Random();
+        public Form1()
+        {
+            InitializeComponent();
+            chart1.Legends[0].Enabled = false;
+            chart1.ChartAreas[0].AxisX.LabelStyle.Format = "F2";
+
+            var area = chart1.ChartAreas[0];
+
+            area.AxisX.Title = "Время";
+            area.AxisX.TitleFont = new Font("Arial", 10);
+
+            area.AxisY.Title = "Количество заявок в очереди";
+            area.AxisY.TitleFont = new Font("Arial", 10);
+        }
+
+        private void start_button_Click(object sender, EventArgs e)
+        {
+            foreach (var series in chart1.Series)
+            {
+                series.Points.Clear();
+            }
+            double lambda = (double)lambda_form.Value; // Интенсивность входящего потока
+            double mu = (double)mu_form.Value;     // Интенсивность обслуживания
+            int totalRequests = (int)N_form.Value; // Количество заявок для моделирования
+
+            if (lambda >= mu)
+            {
+                MessageBox.Show("Система будет перегружена, так как lambda >= mu");
+            }
+
+            double currentTime = 0; // текущее время
+            double nextArrival = 0; // слудующее прибытие
+            double nextCompletion = double.MaxValue; // время ухода текущего 
+
+            int queueCount = 0; // очередь
+            int processedRequests = 0; // подсчет обработанных заявок
+
+            // Статистика
+            double sumWaitTime = 0;         // Сумма времени ожидания всех заявок
+            double sumTimeInSystem = 0;     // Сумма времени нахождения в системе
+            double areaQueue = 0;           // Для средней длины очереди
+            double lastEventTime = 0;       // Время предыдущего события
+
+            listBox1.Items.Clear();
+            listBox1.Items.Add("Статистика:");
+
+            // Очередь для хранения времени прихода каждой заявки
+            Queue<double> arrivalTimes = new Queue<double>();
+
+            while (processedRequests < totalRequests)
+            {
+                areaQueue += queueCount * (currentTime - lastEventTime);
+                lastEventTime = currentTime;
+                
+                if (nextArrival < nextCompletion)
+                {
+                    // Приход заявки
+                    currentTime = nextArrival;
+
+                    if (nextCompletion == double.MaxValue) // Канал свободен
+                    {
+                        double serviceTime = ExpTime(mu);
+                        nextCompletion = currentTime + serviceTime;
+                        sumTimeInSystem += serviceTime;
+                    }
+                    else // Канал занят, встаем в очередь
+                    {
+                        queueCount++;
+                        arrivalTimes.Enqueue(currentTime);
+                        chart1.Series[0].Points.AddXY(currentTime, queueCount);
+                    }
+
+                    // Планируем следующий приход
+                    nextArrival = currentTime + ExpTime(lambda);
+                }
+                else
+                {
+                    // Завершение обслуживания
+                    currentTime = nextCompletion;
+                    processedRequests++;
+
+                    if (queueCount > 0)
+                    {
+                        // Берем человека из очереди
+                        double arrival = arrivalTimes.Dequeue();
+                        double waitTime = currentTime - arrival; // Сколько он простоял в очереди
+                        sumWaitTime += waitTime;
+
+                        double serviceTime = ExpTime(mu);
+                        sumTimeInSystem += (waitTime + serviceTime); // Время в очереди + время обслуживания
+
+                        queueCount--;
+                        chart1.Series[0].Points.AddXY(currentTime, queueCount);
+                        nextCompletion = currentTime + serviceTime;
+                    }
+                    else
+                    {
+                        nextCompletion = double.MaxValue; // Канал освободился
+                    }
+                }
+            }
+
+            areaQueue += queueCount * (currentTime - lastEventTime);
+            double avgWaitTime = sumWaitTime / totalRequests; 
+            double avgTimeInSystem = sumTimeInSystem / totalRequests; 
+            double avgQueueLength = areaQueue / currentTime; 
+            double avgSystemLength = avgQueueLength + (lambda / mu); 
+
+            listBox1.Items.Add($"Среднее время ожидания: {avgWaitTime:F3} сек.");
+            listBox1.Items.Add($"Среднее время в системе: {avgTimeInSystem:F3} сек.");
+            listBox1.Items.Add($"Средняя длина очереди: {avgQueueLength:F3} чел.");
+            listBox1.Items.Add($"Среднее число заявок в системе: {avgSystemLength:F3} чел.");
+            listBox1.Items.Add($"Загрузка системы (ро): {lambda / mu:P2}");
+        }
+
+        // Генерация случайной величины с экспоненциальным распределением
+        private double ExpTime(double rate)
+        {
+            return -Math.Log(1.0 - rnd.NextDouble()) / rate;
+        }
+
+        private void button1_Click(object sender, EventArgs e)
+        {
+            this.Close();
+        }
+    }
+}
